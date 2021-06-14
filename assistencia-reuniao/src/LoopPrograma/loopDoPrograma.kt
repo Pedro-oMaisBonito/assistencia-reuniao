f6eef5321b29fa281c9cb44972f22acd7c3e8c98
Pedro-oMaisBonito <83978995+Pedro-oMaisBonito@users.noreply.github.com>
2021-06-14 07:23:43 -0300

    Update loopDoPrograma.kt

--- assistencia-reuniao/src/LoopPrograma/loopDoPrograma.kt
+++ assistencia-reuniao/src/LoopPrograma/loopDoPrograma.kt
@@ -1,6 +1,7 @@
 // Essa é a função para fazer o programa dar um loop.
 // Criei um variavel chama da 'fim', com o valor 0. O programa ficará em loop até o valor mudar.
 // Por isso no final de cada operação (a não ser fechar o programa), eu pergunto se o usuário quer fechar ou não o progeama.
+
 fun loopDoPrograma() {
     var fim = 0
     while (fim == 0) {
